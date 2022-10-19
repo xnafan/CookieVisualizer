70f63935e35d159d531a2bde643dcad8b7059483
Jakob Farian Krarup <kr@rup.dk>
2022-10-19 21:44:56 +0200

    Add project files.

---
+++ CookieDemonstration/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using CookieDemonstration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace CookieDemonstration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult DeleteCookie([FromQuery] string cookiename)
+        {
+            Response.Cookies.Delete(cookiename);
+            return Redirect(nameof(Index));
+        }
+
+        public IActionResult CreateCookie([FromForm] Cookie newCookie)
+        {
+            Response.Cookies.Append(newCookie.CookieName, newCookie.CookieValue);
+            return Redirect(nameof(Index));
+        }
+    }
+}
---
+++ CookieDemonstration/Models/Cookie.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CookieDemonstration.Models
+{
+    public class Cookie
+    {
+        public string CookieName { get; set; }
+        public string CookieValue { get; set; }
+
+    }
+}
